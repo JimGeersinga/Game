132c49d8ecf18b2475e4dc249501bd9e0e2aa82c
xsherman <j.geersinga@outlook.com>
2015-01-26 12:01:52 +0100

    added markers google maps

--- Game/Scripts/Game.js
+++ Game/Scripts/Game.js
@@ -51,9 +51,64 @@ $(function () {
         });
         return false;
     };
+   
+    var map;
+    var myLatLng;
+    var markers = [];
 
+    function initialize() {
+        var haightAshbury = new google.maps.LatLng(37.7699298, -122.4469157);
+        var mapOptions = {
+            zoom: 12,
+            center: haightAshbury,
+            mapTypeId: google.maps.MapTypeId.HYBRID
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+
+        google.maps.event.addListener(map, 'click', function (event) {
+            addMarker(event.latLng);
+            var lat = event.latLng.lat();
+            var lng = event.latLng.lng();
+
+            myLatLng = new google.maps.LatLng(lat, lng);
+
+            console.log("Latitude = " + lat);
+            console.log("Longitude = " + lng);
+            console.log("");
+        });
+
+    }
+    function addMarker(location) {
+        deleteMarkers();
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+        markers.push(marker);
+    }
+
+    function setAllMap(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
+
+    function deleteMarkers() {
+        setAllMap(null);
+        markers = [];
+    }
+
+    function calcDistance(p1, p2) {
+        return (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
+    }
+
+    // ------------------------------------------------------ //
+
+    google.maps.event.addDomListener(window, 'load', initialize);
 
     $("form[data-User-Ajax='true']").submit(ajaxFormSubmit);
+
     $("input[data-User-autocomplete]").each(createAutocomplete);
 
     $(".main-content").on("click", ".pagedList a", getPage);
